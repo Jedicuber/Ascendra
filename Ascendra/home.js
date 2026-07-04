996e9b5611436e3d01878df2ef37a33d77e50e36
jaydenwcaau-collab <jaydenwcaau@gmail.com>
2026-07-04 16:43:45 +1000

    Update home.js

--- Ascendra/home.js
+++ Ascendra/home.js
@@ -1,10 +1,18 @@
 //going to try and make a animated fox icon with js
 var y = 200;
 var x = 200;
-draw = function{
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
     background(255, 255, 0);
-    //main head
+
+    // main head
     fill(165, 255, 0);
     ellipse(x, y, 250, 250);
-    //
+
+    //ears
+    triangle()
 }
