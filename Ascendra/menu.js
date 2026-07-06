24cc1d3a6561db31de4dfd461b8ae0ae83df7940
jaydenwcaau-collab <jaydenwcaau@gmail.com>
2026-07-06 10:56:29 +1000

    Update menu.js

--- Ascendra/menu.js
+++ Ascendra/menu.js
@@ -1,26 +1,23 @@
-function goTo(page) {
-    location.window.href=page;
+const buttons = document.querySelectorAll("#nav button");
+
+const radius = 220;   // distance from "Menu"
+let angle = 0;
+
+function animate() {
+    angle += 0.002;   // speed of rotation
+
+    buttons.forEach((button, i) => {
+
+        const a = angle + i * (Math.PI * 2 / buttons.length);
+
+        const x = Math.cos(a) * radius;
+        const y = Math.sin(a) * radius;
+
+        button.style.left = x + "px";
+        button.style.top = y + "px";
+    });
+
+    requestAnimationFrame(animate);
 }
-    const buttons = document.querySelectorAll("#nav button");
 
-    const radius = 220;
-    let angle = 0;
-
-    function animate() {
-        angle += 0.002;
-
-        buttons.forEach((button, i) => {
-
-            const a = angle + i * (Math.PI * 2 / buttons.length);
-
-            const x = Math.cos(a) * radius;
-            const y = Math.sin(a) * radius;
-
-            button.style.left = x + "px";
-            button.style.top = y + "px";
-        });
-
-        requestAnimationFrame(animate);
-    }
-
-    animate();
+animate();
