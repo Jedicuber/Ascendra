f79f2c740cf7f0cb0b6e66214fde4d7346a20c72
ess <alan.kai.chong@gmail.com>
2026-07-05 18:47:00 -0600

    Adjust button animation radius and fix formatting

--- Ascendra/menu.js
+++ Ascendra/menu.js
@@ -1,25 +1,26 @@
 function goTo(page) {
     location.window.href=page;
 }
-const buttons = document.querySelectorAll("#nav button");
+    const buttons = document.querySelectorAll("#nav button");
 
-        const radius = 250;
-        let angle = 0;
+    const radius = 220;
+    let angle = 0;
 
-        function animate() {
-            angle += 0.002;
+    function animate() {
+        angle += 0.002;
 
-            buttons.forEach((button, i) => {
-                const a = angle + i * (Math.PI * 2 / buttons.length);
+        buttons.forEach((button, i) => {
 
-                const x = Math.cos(a) * radius;
-                const y = Math.sin(a) * radius;
+            const a = angle + i * (Math.PI * 2 / buttons.length);
 
-                button.style.left = x + "px";
-                button.style.top = y + "px";
-            });
+            const x = Math.cos(a) * radius;
+            const y = Math.sin(a) * radius;
 
-            requestAnimationFrame(animate);
-        }
+            button.style.left = x + "px";
+            button.style.top = y + "px";
+        });
 
-        animate();
+        requestAnimationFrame(animate);
+    }
+
+    animate();
