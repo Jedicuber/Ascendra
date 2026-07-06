d907cad84a12b106f2d33751c1de0e7ecb63d5ff
ess <alan.kai.chong@gmail.com>
2026-07-05 18:43:37 -0600

    Implement animated circular navigation buttons
    Add animation to navigation buttons using trigonometric functions.

--- Ascendra/menu.js
+++ Ascendra/menu.js
@@ -1,1 +1,22 @@
+ const buttons = document.querySelectorAll("#nav button");
 
+        const radius = 250;
+        let angle = 0;
+
+        function animate() {
+            angle += 0.002;
+
+            buttons.forEach((button, i) => {
+                const a = angle + i * (Math.PI * 2 / buttons.length);
+
+                const x = Math.cos(a) * radius;
+                const y = Math.sin(a) * radius;
+
+                button.style.left = x + "px";
+                button.style.top = y + "px";
+            });
+
+            requestAnimationFrame(animate);
+        }
+
+        animate();
