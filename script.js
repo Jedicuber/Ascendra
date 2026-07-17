117220397e98bf5c92d60585fbf14b656c34a46f
ess <alan.kai.chong@gmail.com>
2026-07-17 13:21:48 -0600

    Change search shortcut to Ctrl+Shift+K

--- script.js
+++ script.js
@@ -2693,10 +2693,7 @@ searchInput.addEventListener("input", function() {
     showSearchResults(matches);
 });
 document.addEventListener("keydown", function(event) {
-    if (
-        (event.ctrlKey || event.metaKey) &&
-        event.key.toLowerCase() === "k"
-    ) {
+    if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() === "k") {
         event.preventDefault();
         openSearch();
     }
